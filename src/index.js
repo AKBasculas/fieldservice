ed2a15754f29a90560c2e4a9398ad4c2a694cbf9
Iván León <ivan@akbasculas.com>
2019-06-08 12:57:40 -0700

    Added dotenv module and configure .env file with PORT variable

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
+import 'dotenv/config'
+
 const express = require('express')
 const app = express()
 const port = 3000
 
+
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(process.env.PORT, () => console.log(`Example app listening on port ${process.env.PORT}!`))
